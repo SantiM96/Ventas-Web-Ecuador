6e4f6ee91651d1e964813d38261bb00bb128ddf5
manuelp1345 <manuelp1345@gmail.com>
2020-08-26 18:45:01 -0400

    Node

--- api/index.js
+++ api/index.js
@@ -1,33 +1,19 @@
 const express= require("express")
-
+const multer = require("multer")
 const articulos = require("./routes/articulos")
 
 const app = express()
 /* app.use(bodyParser.json()) */
 const path = require("path")
-const multer = require("multer")
 
-let storage = multer.diskStorage({
-    destination:(req,file,cb)=>{
-        cb(null,"./img")
-    },
-    filename:(req,file,cb)=>{
-        cb(null,file.fieldname + `-` + Date.now() + path.extname(file.originalname))
-    }
-})
+app.use(multer({
+    dest: path.join(__dirname, "../img")
+}).single("file"))
 
-const upload = multer({storage})
 
-app.get("/",(req,res)=>{
-    res.send("hola mundo")
-    console.log("hola")
-})
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-
-app.post("/api/upload",upload.single("file"), (req,res)=>{
-    console.log("Storage location is"+req.hostname+"/"+req.file.path )
+app.post("/api/upload", (req,res)=>{
+    console.log(req.file)
     return res.send(req.file)
 })
 
